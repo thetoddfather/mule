5c7d160e6775b5525dd553ebe868cdea2d20354d
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-10-18 19:30:46 -0300

    fix

--- tests/test-extensions/heisenberg-extension/src/main/java/org/mule/test/heisenberg/extension/SecureHeisenbergConnectionProvider.java
+++ tests/test-extensions/heisenberg-extension/src/main/java/org/mule/test/heisenberg/extension/SecureHeisenbergConnectionProvider.java
@@ -6,12 +6,23 @@
  */
 package org.mule.test.heisenberg.extension;
 
+import static org.mule.runtime.api.meta.ExternalLibraryType.NATIVE;
+import static org.mule.test.heisenberg.extension.HeisenbergExtension.HEISENBERG_LIB_CLASS_NAME;
+import static org.mule.test.heisenberg.extension.HeisenbergExtension.HEISENBERG_LIB_DESCRIPTION;
+import static org.mule.test.heisenberg.extension.HeisenbergExtension.HEISENBERG_LIB_FILE_NAME;
+import static org.mule.test.heisenberg.extension.HeisenbergExtension.HEISENBERG_LIB_NAME;
 import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.tls.TlsContextFactory;
 import org.mule.runtime.extension.api.annotation.Alias;
+import org.mule.runtime.extension.api.annotation.ExternalLib;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 
 @Alias("secure")
+@ExternalLib(name = HEISENBERG_LIB_NAME,
+    description = HEISENBERG_LIB_DESCRIPTION,
+    nameRegexpMatcher = HEISENBERG_LIB_FILE_NAME,
+    requiredClassName = HEISENBERG_LIB_CLASS_NAME,
+    type = NATIVE)
 public class SecureHeisenbergConnectionProvider extends HeisenbergConnectionProvider {
 
   @Parameter
